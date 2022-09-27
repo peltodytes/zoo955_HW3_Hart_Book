cacd7d2fa7456aa1f7f98ace3a29a9c8fc21d305
Ian Hart <ian.hart167@gmail.com>
2022-09-27 14:30:00 -0500

    checkpoint 1

--- Hart_Book_HW3_ZOO955.R
+++ Hart_Book_HW3_ZOO955.R
@@ -1,2 +1,17 @@
 #Hart & Book HW3 ZOO955
+require(readxl); require(dplyr)
 
+data = read_excel('BSB_tagging_data.xlsx')
+
+hist(data$Length_at_capture)
+hist(log(data$Length_at_capture))
+
+pois_response = rpois(1000, 5) %>% sort()
+pred = rnorm(1000, mean = 4, sd = 1) %>%  sort()
+
+data = as.data.frame(cbind(pois_response, pred)) 
+plot(data$pred, data$pois_response)
+
+logpois_data = log10(pois_data)
+
+glm()
