e483469c62af454278effa0d93c318f2722561ec
Ian Hart <ian.hart167@gmail.com>
2022-10-03 09:35:07 -0500

    final edit

--- code/Hart_Book_HW3_ZOO955.R
+++ code/Hart_Book_HW3_ZOO955.R
@@ -1,69 +1,64 @@
 #Hart & Book HW3 ZOO955
-require(dplyr)
+require(tidyverse)
 
-# intercept
-B0 = 1
-# slope
-B1 = 5
+#create a data frame to store simulation data
+p<- data.frame(matrix(ncol=2, nrow=1000))
+colnames(p)<- c("p.plm","p.llm")
 
-data = data.frame(x = seq(1,100), lambda = NA, y = NA, log10y = NA)
-
-# populating the columns
-for(i in 1:nrow(data)){
-  
-  data$lambda[i] = B0 + B1*(data$x[i])
-  data$y[i] = rpois(1, data$lambda[i])
-  data$log10y[i] = log10(data$y[i])
-  
-}
-
-plot(data$x, data$y)
-plot(data$x, data$log10y)
-
-# not very Poisson-ish
-plot(density(data$y))
-# I thought the variable lambda might account for what we're seeing so I tried to normalize for that. Still looks a little strange
-plot(density(data$y/data$lambda))
-
-# log transformation doesn't seem to make it any more Gaussian
-plot(density(data$log10y))
-
-
-plm = glm(y ~ x, family = 'poisson', data = data)
-# intercept & slope are way off, not sure if this is just from randomness of model or because of a data issue
-summary(plm)
-
-llm = glm(log10y ~ x, family = 'gaussian', data = data)
-# intercept & slope are way off
-summary(llm)
-
-
-# New Attempt
-
-# Data generation borrowed from StackExchange user ocram 
-#sample size
-n <- 100
-#regression coefficients
+#set regression coefficients
 beta0 <- 1
 beta1 <- 0.2
-#generate covariate values
-x <- runif(n=n, min=10, max=11.5)
-#compute mu's
-mu <- exp(beta0 + beta1 * x)
-#generate Y-values
-y <- rpois(n=n, lambda=mu)
-#data set
-data <- data.frame(y=y, x=x, logy=log(y))
-data
 
-plot(density(data$y))
-plot(density(log(data$y)))
-plot(density(data$logy))
+#make a function to simulate data, run models, and calculate probability of rejecting null
+prob.rej.null<- function(n){
+  for(i in 1:nrow(p)){
+    
+    #generate covariate values
+    x <- runif(n=n, min=10, max=11.5)
+    #compute mu's
+    mu<- exp(beta0 + beta1 * x)
+    #generate Y-values
+    y <- rpois(n=n, lambda=mu)
+    #organize generated data in data frame
+    data <- data.frame(y=y, x=x, logy=log(y))
+    #generalized linear model on un-transformed count data
+    plm <- glm(y ~ x, family = poisson, data = data)
+    #linear model on log-transformed count data
+    llm <- glm(logy ~ x, family = gaussian, data = data)
+    #extract p-values from both model types
+    p$p.plm[i]<- summary(plm)$coefficients[2,4]
+    p$p.llm[i]<- summary(llm)$coefficients[2,4]
+    
+  }
+  #calculate the number of times out of 1000 where the null was rejected
+  #at alpha 0.05
+  prob.plm<- sum(p$p.plm <0.05)/1000
+  prob.llm<- sum(p$p.llm <0.05)/1000
+  #probability of rejecting the null is the function output
+  return(list(prob.plm,prob.llm))
+}
 
-plm = glm(y ~ x, family = poisson, data = data)
-summary(plm)
+#make a data frame to hold outcomes of simulations with different sample sizes
+comp<- data.frame(n=c(25,50,100,150,500), prob.plm=NA, prob.llm=NA)
 
-llm = glm(logy ~ x, family = gaussian, data = data)
-summary(llm)
+for(i in 1:nrow(comp)){
+  #run function over 5 n levels and store probs for two model types as a list
+  list<- prob.rej.null(comp$n[i])
+  #extract first probability in list
+  comp$prob.plm[i]<- as.numeric(unlist(list[1]))
+  #extract second probability in list
+  comp$prob.llm[i]<- as.numeric(unlist(list[2]))
+}
 
-llm$coefficients
+#plot
+comp%>%
+  pivot_longer(cols=c("prob.plm","prob.llm"),names_to="mod.type",values_to="prob")%>%
+  ggplot(aes(x=n, y=prob,color=mod.type))+
+  geom_jitter(size=3,width=3)+
+  scale_y_continuous(limits=c(0,1))+
+  xlab("Sample Size")+
+  ylab("Probability of Rejecting Null")+
+  scale_color_manual(name="Model Type",
+                     labels=c("Linear","Poisson"),
+                     values=c("purple","green"))+
+  theme_bw()
